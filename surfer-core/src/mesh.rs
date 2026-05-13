888bfab9c666499014abbf1b4ba4fa090d60ae8c
Garth Trickett <garthtrickett@gmail.com>
2026-05-13 11:43:20 +1000

    Fix missing `active` field in OutlineLayer struct initialization within tests

--- surfer-core/src/mesh.rs
+++ surfer-core/src/mesh.rs
@@ -1791,8 +1791,9 @@ mod tests {
             tangents2: vec![Vec3::new(12.0, 0.0, 75.0), Vec3::new(12.0, 0.0, 80.0)],
             ..Default::default()
         };
-        model.outline_layers = Some(vec![OutlineLayer {
+                model.outline_layers = Some(vec![OutlineLayer {
             name: "Wing".to_string(),
+            active: true,
             otl_ext: wing_ext,
             otl_int: BezierCurveData::default(),
         }]);
