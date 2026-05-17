9983647b5dedd56ea13438831cbefe76cca76088
Garth Trickett <garthtrickett@gmail.com>
2026-05-17 16:38:44 +1000

    Disable unsafe-any lint rules for web-test-runner.config.js

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { playwrightLauncher } from '@web/test-runner-playwright';
 
